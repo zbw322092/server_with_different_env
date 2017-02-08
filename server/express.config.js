ea754bbedc23ea139dfb3a6bab0198b145d03dc8
Bowen Zhong <bz4e14@soton.ac.uk>
2017-02-08 23:11:44 +0800

    initial babel setting

---
+++ server/express.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const chalk = require('chalk');
+const express = require('express');
+var exphbs  = require('express-handlebars');
+var app = express();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var cookieParser = require('cookie-parser');
+var helpers = require('handlebars-helpers')();
+
+var env = app.get('env');
+
+app.use(morgan('dev'))
+   .use(bodyParser({extended: false}))
+   .use(bodyParser.json())
+   .use(methodOverride())
+   .use(cookieParser());
+
+app.engine('.hbs', exphbs({
+	defaultLayout: 'main',
+	layoutsDir: 'server/views/layouts',
+	partialsDir: 'server/views/partials/',
+	extname: '.hbs',
+	helpers: helpers
+}));
+
+app.set('view engine', '.hbs');
+
+app.set('views', process.cwd() + '/server/views/');
+
+app.get('/test', function(req, res) {
+	res.render('home');
+});
+
+app.get('/about', function(req, res) {
+	res.sendFile(path.join(__dirname, '../client/abouttest/views/about.html'));
+});
+
+app.listen(process.env.PORT, function() {
+	console.log(chalk.green('server is listening on port ' + process.env.PORT));
+});
+
--- server/index.js
+++ server/index.js
@@ -1,43 +1,7 @@
-const path = require('path');
-const chalk = require('chalk');
-const express = require('express');
-var exphbs  = require('express-handlebars');
-var app = express();
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
-var cookieParser = require('cookie-parser');
-var helpers = require('handlebars-helpers')();
+'use strict';
 
-var env = app.get('env');
+var nodeEnv = process.env.NODE_ENV || 'development';
 
-app.use(morgan('dev'))
-   .use(bodyParser({extended: false}))
-   .use(bodyParser.json())
-   .use(methodOverride())
-   .use(cookieParser());
-
-app.engine('.hbs', exphbs({
-	defaultLayout: 'main',
-	layoutsDir: 'server/views/layouts',
-	partialsDir: 'server/views/partials/',
-	extname: '.hbs',
-	helpers: helpers
-}));
-
-app.set('view engine', '.hbs');
-
-app.set('views', process.cwd() + '/server/views/');
-
-app.get('/test', function(req, res) {
-	res.render('home');
-});
-
-app.get('/about', function(req, res) {
-	res.sendFile(path.join(__dirname, '../client/abouttest/views/about.html'));
-});
-
-app.listen(process.env.PORT, function() {
-	console.log(chalk.green('server is listening on port ' + process.env.PORT));
-});
-
+if (nodeEnv === 'development' || nodeEnv === 'test') {
+	require('babel-register');
+}
